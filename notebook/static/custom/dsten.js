b242f9bf8a84c03bea32587af2377d28806f8105
John DeNero <papajohn@gmail.com>
2015-08-19 09:51:56 -0700

    simple -> student

--- notebook/static/custom/dsten.js
+++ notebook/static/custom/dsten.js
@@ -1,21 +1,17 @@
 /*
 
-Customized for Data Science 10
-course created and taught by Professor John DeNero
+Customized for Data Science 8
 
-Professor: denero.org
-Course: dsten.github.io
+Toggle on-off "Student Mode" using the check mark in the toolbar.
 
-Toggle on-off "Simple Mode" using the check mark in the toolbar.
+Student Mode contains the following modifications:
 
-Simple Mode contains the following modifications:
-
-1. By default, shift-enter runs all cells.
-2. Only edit mode exists:
+1. If a cell is executed out of order, then all above are executed as well
+2. Only edit mode exists, and only for code cells:
 	a. All command mode shortcuts are removed.
 	b. The first code cell is selected upon page load.
-	c. Markdown-formatted cells are automatically editable upon hover.
-3. Small "scratch cell" modal is available for testing code outside of the current file.
+	c. Markdown-formatted cells are not editable
+3. "scratch cell" modal for testing code outside of the current environment.
 
 @author: Alvin Wan
 @site: alvinwan.com
@@ -28,26 +24,26 @@ define([
 
 		/*
 
-		Simple Mode Button
+		Student Mode Button
 
 		- adds a button to the notebook toolbar
-		- toggles a global simple mode variable
+		- toggles a global student mode variable
 
 		icon: fortawesome.github.io/Font-Awesome/icons
 
 		*/
-		
-		SIMPLE_CELL_CLASS = 'simple-mode-cell';
-		
+
+		STUDENT_CELL_CLASS = 'student-mode-cell';
+
 		IPython.toolbar.add_buttons_group([{
-			'label': 'Simple Mode Toggle',
+			'label': 'Student Mode Toggle',
 			'icon': 'fa-square-o',
 			'callback': function() {
 				var button = $(this).children('i');
 				button.toggleClass('fa-square-o').toggleClass('fa-check-square-o');
-				window.simple = button.hasClass('fa-check-square-o');
-				button.children('b').html(window.simple ? 'on' : 'off');
-				if (window.simple) remove_command_mode();
+				window.student = button.hasClass('fa-check-square-o');
+				button.children('b').html(window.student ? 'on' : 'off');
+				if (window.student) remove_command_mode();
 				else restore_command_mode();
 				toggle([
 						'.input_prompt',
@@ -66,15 +62,15 @@ define([
 					'display', 'none', 'inline-block');
 				toggle([
 					'.btn-group[id="ok_tests"]',
-					'.'+SIMPLE_CELL_CLASS,
-					'.simple_modal'
+					'.'+STUDENT_CELL_CLASS,
+					'.student_modal'
 				], 'display', 'inline-block', 'none');
 			}
 		}]);
 
 		// toggle specified CSS attribute between two options
 		function toggle(list, attr, option1, option2) {
-			var option = window.simple ? option1 : option2;
+			var option = window.student ? option1 : option2;
 			for (var i in list) {
 				item = list[i];
 				$(item).css(attr, option);
@@ -83,30 +79,30 @@ define([
 
 		/*
 
-		Simple Mode Setup
+		Student Mode Setup
 
-		- add a label to the DS10 Button
+		- add a label to the student-model button
 		- select the first cell on load
 
 		*/
 
-		function initialize_simple_mode() {
-			$('.fa-square-o').html('	Simple mode <b>on</b>').click()
+		function initialize_student_mode() {
+			$('.fa-square-o').html('	Student mode <b>on</b>').click()
 				.parents('.btn-group').css('float', 'right');
 			select_cell($('.cell:first-child'));
 		}
-		
+
 		function pick_cell() {
-			if ($('.'+SIMPLE_CELL_CLASS).length == 0) {
+			if ($('.'+STUDENT_CELL_CLASS).length == 0) {
 				candidate = next_available_code_cell();
-				candidate.addClass(SIMPLE_CELL_CLASS);
+				candidate.addClass(STUDENT_CELL_CLASS);
 			}
 		}
-		
+
 		function unpick_cell() {
-			$('.'+SIMPLE_CELL_CLASS).remove();
+			$('.'+STUDENT_CELL_CLASS).remove();
 		}
-		
+
 		/*
 
 		Ok Test Button
@@ -118,13 +114,13 @@ define([
 		*/
 
 		IPython.toolbar.add_buttons_group([{
-			'label': 'Run ok tests',
+			'label': 'Run tests',
 			'icon': 'fa-user-secret',
 			'callback': function() {
 				run_script("import subprocess\nsubprocess.call(['python3', 'ok', '--extension', 'notebook'])");
 			}
 		}]);
-		
+
 		/*
 
 		OK Test Setup
@@ -134,7 +130,7 @@ define([
 		*/
 
 		function initialize_ok_tests() {
-			$('.fa-user-secret').html('	   Run ok tests')
+			$('.fa-user-secret').html('	   Run tests')
 				.parents('.btn-group').css('float', 'right').attr('id', 'ok_tests');
 		}
 
@@ -156,19 +152,19 @@ define([
 			freeze_object('edits', edt);
 			cmd.clear_shortcuts();
 			edt.clear_shortcuts();
-			add_simple_shortcuts();
-			$('.simple_modal').show();
-			$('#simple_mode').attr('rel', 'stylesheet');
+			add_student_shortcuts();
+			$('.student_modal').show();
+			$('#student_mode').attr('rel', 'stylesheet');
 		}
 
 		function restore_command_mode() {
 			restore_object('commands', cmd);
 			restore_object('edits', edt);
-			$('.simple_modal').hide();
-			$('#simple_mode').attr('rel', 'simple-mode-deactivated');
+			$('.student_modal').hide();
+			$('#student_mode').attr('rel', 'student-mode-deactivated');
 		}
 
-		function add_simple_shortcuts() {
+		function add_student_shortcuts() {
 			edt.add_shortcut('shift-enter', function() {
 				if (window.modal) {
 					run($('.modal_cell'));
@@ -198,20 +194,20 @@ define([
 		/*
 
 		Utilities
-		
+
 		- Preventing Other Modes
 			- destroy all "command mode" shortcuts
 			- make text cell editable on hover
 			- select the first cell on load (buggy)
 		- Running Shell Script
 			- if a cell is empty
-		
+
 		*/
 
 		// Preventing Other Modes
-		
+
 		$(document).on('mouseenter', '.text_cell', function() {
-			if (window.simple) keep_state(select_cell, this);
+			if (window.student) keep_state(select_cell, this);
 		});
 
 		function keep_state(func, arg) {
@@ -228,15 +224,15 @@ define([
 			$(self).click();
 			$('#run_cell').click();
 		}
-		
+
 		// Running Shell Script
-		
+
 		function run_script(script) {
 			console.log('[Notebook] executing: '+script);
 			var kernel = IPython.notebook.kernel;
             kernel.execute(script);
 		}
-		
+
 		// TODO: a code cell with one character is "empty", according to this but empty has length of 1 0.o
 		function is_empty(code_cell) {
 			container = code_cell.find('.CodeMirror-code').children('pre').children('span');
@@ -251,10 +247,10 @@ define([
 		function next_available_cell(selector) {
 			candidate = $(selector);
 			if (is_empty(candidate)) {
-				console.log('[Simple Mode] Cell empty. Selecting cell.');
+				console.log('[Student Mode] Cell empty. Selecting cell.');
 				return candidate;
 			} else {
-				console.log('[Simple Mode] Cell not empty. Adding new cell.');
+				console.log('[Student Mode] Cell not empty. Adding new cell.');
 				select_cell($('.cell:last-child'));
 				$('#insert_cell_below').click();
 				return next_available_code_cell();
@@ -271,33 +267,33 @@ define([
 
 		*/
 
-		function initialize_simple_modal() {
+		function initialize_student_modal() {
 			window.modal = false;
 			$('#insert-cell-below').click();
 		}
 
-		window.toggle_simple_modal = function() {
-			if ($('.'+SIMPLE_CELL_CLASS).length > 0) unpick_cell(); else pick_cell();
-			$('.'+SIMPLE_CELL_CLASS).toggleClass('shown');
-			$('.simple_modal').toggleClass('shown');
-			window.modal = $('.'+SIMPLE_CELL_CLASS).hasClass('shown');
-			$('.simple_modal .button').html(window.modal ? 'Deactivate' : 'Activate');
+		window.toggle_student_modal = function() {
+			if ($('.'+STUDENT_CELL_CLASS).length > 0) unpick_cell(); else pick_cell();
+			$('.'+STUDENT_CELL_CLASS).toggleClass('shown');
+			$('.student_modal').toggleClass('shown');
+			window.modal = $('.'+STUDENT_CELL_CLASS).hasClass('shown');
+			$('.student_modal .button').html(window.modal ? 'Deactivate' : 'Activate');
 		}
 
 		$(document).ready(function() {
-			
+
 			// JS initializers
-			
-			initialize_simple_mode();
-			initialize_simple_modal();
+
+			initialize_student_mode();
+			initialize_student_modal();
 			initialize_ok_tests();
-			
+
 			// DOM initializers
-			
-			$('head').append('<link href="/static/custom/dsten.css" rel="stylesheet" id="simple_mode">');
+
+			$('head').append('<link href="/static/custom/dsten.css" rel="stylesheet" id="student_mode">');
 			$('head').append('<script src="https://raw.githubusercontent.com/dwachss/bililiteRange/master/bililiteRange.js"></script>');
 			$('head').append('<script src="https://raw.githubusercontent.com/dwachss/bililiteRange/master/jquery.sendkeys.js"></script>');
-			$('#notebook').append('<div class="simple_modal"><div class="simple_text"><h3>Scratch Cell</h3>' + '<p>"Scratch" offers a small sandbox environment, independent of your IPython notebook. ' + 'Shift+Enter with the Scratch Cell open to run it.</div><div class="button" ' + 'onclick="toggle_simple_modal()">Activate</div></div>');
+			$('#notebook').append('<div class="student_modal"><div class="student_text"><h3>Scratch Cell</h3>' + '<p>"Scratch" offers a small sandbox environment, independent of your IPython notebook. ' + 'Shift+Enter with the Scratch Cell open to run it.</div><div class="button" ' + 'onclick="toggle_student_modal()">Activate</div></div>');
 		});
 	});
 });
